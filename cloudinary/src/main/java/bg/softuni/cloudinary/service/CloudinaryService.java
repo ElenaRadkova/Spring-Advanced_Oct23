8e555a3d6c5c704348f6615b642f06b8a340291c
Elena Kanazireva <122015420+ElenaRadkova@users.noreply.github.com>
2023-12-04 21:44:33 +0200

    Delete cloudinary directory

--- cloudinary/src/main/java/bg/softuni/cloudinary/service/CloudinaryService.java
+++
@@ -1,8 +0,0 @@
-package bg.softuni.cloudinary.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface CloudinaryService {
-    CloudinaryImage upload(MultipartFile file);
-    boolean delete(String publicId);
-}
